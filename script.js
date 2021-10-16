3dcc949580b3f904ad5b2181628d8af934fa7873
matias zazzali <matizazzali@gmail.com>
2021-10-16 07:57:54 +0200

    add information

--- script.js
+++ script.js
@@ -41,12 +41,12 @@ $(document).ready(function(){
         loop: true
     });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["Bioengineer", "Data Scientist", "Data Analyst"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    // var typed = new Typed(".typing-2", {
+    //     strings: ["Bioengineer", "Data Scientist", "Data Analyst"],
+    //     typeSpeed: 100,
+    //     backSpeed: 60,
+    //     loop: true
+    // });
 
     // owl carousel script
     $('.carousel').owlCarousel({
